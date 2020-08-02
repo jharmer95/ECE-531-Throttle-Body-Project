def7fe7dfe4fa4429b2132fdb3143e4ccb2337cb
Jackson Harmer <jharmer95@gmail.com>
2020-08-02 03:59:02 -0400

    Adding basic I2C Python code for controller

---
+++ controller/i2c_comms.py
@@ -0,0 +1,126 @@
+"""Module for communicating with the Arduino via I2c and a custom function buffer"""
+
+from enum import IntEnum, unique
+from typing import Dict, List
+import simple_i2c as si2c
+import struct, sys
+
+# Globals
+ADDR = 0x08  # bus address
+
+
+@unique
+class Function(IntEnum):
+    FUNC_GET_SERVO = 1
+    FUNC_SET_SERVO = 2
+
+
+@unique
+class ErrorCode(IntEnum):
+    ERROR_NONE = 0
+    ERROR_GENERIC = -1
+
+
+class Buffer:
+    cmd: int = 0
+    params: Dict[str, List] = {
+        "ints": [0, 0, 0, 0, 0, 0, 0],
+        "floats": [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
+        "string": "",
+    }
+    union_type: str = "int"
+
+    def __init__(self, cmd):
+        self.cmd = cmd
+        self.params = {
+            "ints": [0, 0, 0, 0, 0, 0, 0],
+            "floats": [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
+            "string": "",
+        }
+        self.union_type = "int"
+
+    def pack(self) -> bytes:
+        if self.union_type == "void":
+            data = struct.pack("<b", self.cmd)
+            while len(data) < 28:
+                data += bytes([0])
+            return data
+        elif self.union_type == "int":
+            return struct.pack("<biiiiiii", self.cmd, *self.params["ints"][0:7])
+        elif self.union_type == "uint":
+            return struct.pack("<bIIIIIII", self.cmd, *self.params["ints"][0:7])
+        elif self.union_type == "float":
+            return struct.pack("<bfffffff", self.cmd, *self.params["floats"][0:7])
+        elif self.union_type == "string":
+            data = struct.pack(
+                "<bs", self.cmd, self.params["string"].encode("utf-8")[0:27]
+            )
+            while len(data) < 28:
+                data += bytes([0])
+            return data
+        else:
+            return bytes()
+
+    @classmethod
+    def unpack(cls, data: bytes, u_type: str):
+        if u_type == "void":
+            [cmd] = struct.unpack("<b", data[0:1])
+            inst = cls(cmd)
+            inst.union_type = "void"
+            return inst
+        elif u_type == "int":
+            [cmd, i1, i2, i3, i4, i5, i6, i7] = struct.unpack("<biiiiiii", data)
+            inst = cls(cmd)
+            inst.params["ints"] = [i1, i2, i3, i4, i5, i6, i7]
+            inst.union_type = "int"
+            return inst
+        elif u_type == "uint":
+            [cmd, i1, i2, i3, i4, i5, i6, i7] = struct.unpack("<bIIIIIII", data)
+            inst = cls(cmd)
+            inst.params["ints"] = [i1, i2, i3, i4, i5, i6, i7]
+            inst.union_type = "uint"
+            return inst
+        elif u_type == "float":
+            [cmd, i1, i2, i3, i4, i5, i6, i7] = struct.unpack("<bfffffff", data)
+            inst = cls(cmd)
+            inst.params["floats"] = [i1, i2, i3, i4, i5, i6, i7]
+            inst.union_type = "float"
+            return inst
+        elif u_type == "string":
+            [cmd] = struct.unpack("<b", data[0:1])
+            s = data[1:28].decode("utf-8")
+            inst = cls(cmd)
+            inst.params["string"] = s
+            inst.union_type = "string"
+            return inst
+
+
+def call_function(function: Function, *args):
+    global ADDR
+
+    mesg_buf = Buffer(function)
+    return_type: str = "int"
+
+    if function == Function.FUNC_GET_SERVO:
+        pass
+    elif function == Function.FUNC_SET_SERVO:
+        mesg_buf.params["ints"][0] = args[0]
+        mesg_buf.union_type = "int"
+        return_type = "void"
+
+    si2c.write_bytes(ADDR, mesg_buf.pack())
+
+    response = si2c.read_bytes(ADDR, 29)
+    mesg_buf = Buffer.unpack(response, return_type)
+
+    if mesg_buf.cmd != ErrorCode.ERROR_NONE:
+        mesg_buf = Buffer.unpack(response, "string")
+        print(f"Error {mesg_buf.cmd}: {mesg_buf.params['string']}")
+        return (False, mesg_buf.cmd)
+    
+    if return_type == "void":
+        return (True, None)
+    elif return_type == "int" or return_type == "uint":
+        return (True, mesg_buf.params["ints"][0])
+    elif return_type == "float":
+        return (True, mesg_buf.params["floats"][0])
---
+++ controller/simple_i2c.py
@@ -0,0 +1,230 @@
+"""Module for simplifying I2C communication using smbus2"""
+
+from smbus2 import SMBus, i2c_msg
+import struct, sys
+
+_SMBUS_ACTIVE: bool = False
+_SMBUS_OBJ: SMBus = None
+
+
+def init_bus(num: int) -> bool:
+    global _SMBUS_ACTIVE
+    global _SMBUS_OBJ
+
+    if _SMBUS_ACTIVE:
+        print(
+            "Bus is already active, please close the bus before re-initializing",
+            file=sys.stderr,
+        )
+        return False
+
+    _SMBUS_OBJ = SMBus(num)
+    _SMBUS_ACTIVE = True
+    return True
+
+
+def close_bus():
+    global _SMBUS_ACTIVE
+    global _SMBUS_OBJ
+
+    _SMBUS_OBJ.close()
+    _SMBUS_ACTIVE = False
+
+
+def write_bytes(address: int, data: bytes):
+    global _SMBUS_ACTIVE
+    global _SMBUS_OBJ
+
+    if not _SMBUS_ACTIVE:
+        print("Bus is not active, please initialize the bus first", file=sys.stderr)
+        return
+
+    msg: i2c_msg = i2c_msg.write(address, data)
+    _SMBUS_OBJ.i2c_rdwr(msg)
+
+
+def write_int(
+    address: int,
+    value: int,
+    int_sz: int = 32,
+    signed: bool = True,
+    byte_order: str = "little",
+):
+    global _SMBUS_ACTIVE
+    global _SMBUS_OBJ
+
+    if not _SMBUS_ACTIVE:
+        print("Bus is not active, please initialize the bus first", file=sys.stderr)
+        return
+
+    if int_sz < 0:
+        print("Integer size must be positive", file=sys.stderr)
+        return
+    elif int_sz > 64:
+        print("Max integer size is 64-bit", file=sys.stderr)
+        return
+    elif int_sz not in [8, 16, 32, 64]:
+        print("Irregular integer size, rounding up...")
+        if int_sz < 8:
+            int_sz = 8
+        elif int_sz < 16:
+            int_sz = 16
+        elif int_sz < 32:
+            int_sz = 32
+        elif int_sz < 64:
+            int_sz = 64
+
+    msg: i2c_msg = i2c_msg.write(
+        address, value.to_bytes(int_sz // 8, byteorder=byte_order, signed=signed)
+    )
+    _SMBUS_OBJ.i2c_rdwr(msg)
+
+
+def write_float(address: int, value: float, double_precision: bool = False):
+    global _SMBUS_ACTIVE
+    global _SMBUS_OBJ
+
+    if not _SMBUS_ACTIVE:
+        print("Bus is not active, please initialize the bus first", file=sys.stderr)
+        return
+
+    data: bytes
+    if double_precision:
+        data = struct.pack("d", value)
+    else:
+        data = struct.pack("f", value)
+
+    msg: i2c_msg = i2c_msg.write(address, data)
+    _SMBUS_OBJ.i2c_rdwr(msg)
+
+
+def write_int8(address: int, value: int, byte_order: str = "little"):
+    write_int(address, value, 8, byte_order=byte_order)
+
+
+def write_int16(address: int, value: int, byte_order: str = "little"):
+    write_int(address, value, 16, byte_order=byte_order)
+
+
+def write_int32(address: int, value: int, byte_order: str = "little"):
+    write_int(address, value, 32, byte_order=byte_order)
+
+
+def write_int64(address: int, value: int, byte_order: str = "little"):
+    write_int(address, value, 64, byte_order=byte_order)
+
+
+def write_uint8(address: int, value: int, byte_order: str = "little"):
+    write_int(address, value, 8, signed=False, byte_order=byte_order)
+
+
+def write_uint16(address: int, value: int, byte_order: str = "little"):
+    write_int(address, value, 16, signed=False, byte_order=byte_order)
+
+
+def write_uint32(address: int, value: int, byte_order: str = "little"):
+    write_int(address, value, 32, signed=False, byte_order=byte_order)
+
+
+def write_uint64(address: int, value: int, byte_order: str = "little"):
+    write_int(address, value, 64, signed=False, byte_order=byte_order)
+
+
+def read_bytes(address: int, num_bytes: int) -> bytes:
+    global _SMBUS_ACTIVE
+    global _SMBUS_OBJ
+
+    if not _SMBUS_ACTIVE:
+        print("Bus is not active, please initialize the bus first", file=sys.stderr)
+        return
+
+    msg: i2c_msg = i2c_msg.read(address, num_bytes)
+    _SMBUS_OBJ.i2c_rdwr(msg)
+    return bytes(msg)
+
+
+def read_int(
+    address: int, int_sz: int = 32, signed: bool = True, byte_order: str = "little"
+) -> int:
+    global _SMBUS_ACTIVE
+    global _SMBUS_OBJ
+
+    if not _SMBUS_ACTIVE:
+        print("Bus is not active, please initialize the bus first", file=sys.stderr)
+        return
+
+    if int_sz < 0:
+        print("Integer size must be positive", file=sys.stderr)
+        return
+    elif int_sz > 64:
+        print("Max integer size is 64-bit", file=sys.stderr)
+        return
+    elif int_sz not in [8, 16, 32, 64]:
+        print("Irregular integer size, rounding up...")
+        if int_sz < 8:
+            int_sz = 8
+        elif int_sz < 16:
+            int_sz = 16
+        elif int_sz < 32:
+            int_sz = 32
+        elif int_sz < 64:
+            int_sz = 64
+
+    msg: i2c_msg = i2c_msg.read(address, int_sz // 8)
+    _SMBUS_OBJ.i2c_rdwr(msg)
+    return int.from_bytes(bytes(msg), byteorder=byte_order, signed=signed)
+
+
+def read_float(address: int, double_precision: bool = False) -> float:
+    global _SMBUS_ACTIVE
+    global _SMBUS_OBJ
+
+    if not _SMBUS_ACTIVE:
+        print("Bus is not active, please initialize the bus first", file=sys.stderr)
+        return
+
+    sz: int
+    fmt: str
+    if double_precision:
+        sz = 8
+        fmt = "d"
+    else:
+        sz = 4
+        fmt = "f"
+
+    msg = i2c_msg.read(address, sz)
+    _SMBUS_OBJ.i2c_rdwr(msg)
+    [x] = struct.unpack(fmt, bytes(msg))
+    return x
+
+
+def read_int8(address: int, byte_order: str = "little") -> int:
+    return read_int(address, 8, byte_order=byte_order)
+
+
+def read_int16(address: int, byte_order: str = "little") -> int:
+    return read_int(address, 16, byte_order=byte_order)
+
+
+def read_int32(address: int, byte_order: str = "little") -> int:
+    return read_int(address, 32, byte_order=byte_order)
+
+
+def read_int64(address: int, byte_order: str = "little") -> int:
+    return read_int(address, 64, byte_order=byte_order)
+
+
+def read_uint8(address: int, byte_order: str = "little") -> int:
+    return read_int(address, 8, signed=False, byte_order=byte_order)
+
+
+def read_uint16(address: int, byte_order: str = "little") -> int:
+    return read_int(address, 16, signed=False, byte_order=byte_order)
+
+
+def read_uint32(address: int, byte_order: str = "little") -> int:
+    return read_int(address, 32, signed=False, byte_order=byte_order)
+
+
+def read_uint64(address: int, byte_order: str = "little") -> int:
+    return read_int(address, 64, signed=False, byte_order=byte_order)
